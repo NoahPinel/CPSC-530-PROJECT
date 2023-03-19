477161b88b6baaf3807937da2fb1f8084b54b6a2
Aidenwjt <aidenwjt@gmail.com>
2023-03-18 21:58:09 -0600

    Frequency table/dictionary and Binary tree structs

--- code/bccbt.c
+++ code/bccbt.c
@@ -1,12 +1,132 @@
-// TODO:
-// - Encoding Portion
-// - Decoding Portion
-// - Find other lossless algorithm (pseudocode says we need another one?)
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define DEBUG 1
+
+// ==================================================================================================
+// Key Value Dictionary implementation copied from:
+// https://gist.githubusercontent.com/kylef/86784/raw/fe97567ec9baf5c0dce3c7fcbec948e21dfcce09/dict.c
+
+typedef struct dict_t_struct {
+	char *key;
+	void *value;
+	struct dict_t_struct *next;
+} dict_t;
+
+dict_t **dictAlloc(void) {
+	return malloc(sizeof(dict_t));
+}
+
+void dictDealloc(dict_t **dict) {
+	free(dict);
+}
+
+void *getItem(dict_t *dict, char *key) {
+	dict_t *ptr;
+	for (ptr = dict; ptr != NULL; ptr = ptr->next) {
+		if (strcmp(ptr->key, key) == 0) {
+			return ptr->value;
+		}
+	}
+
+	return NULL;
+}
+
+void delItem(dict_t **dict, char *key) {
+	dict_t *ptr, *prev;
+	for (ptr = *dict, prev = NULL; ptr != NULL; prev = ptr, ptr = ptr->next) {
+		if (strcmp(ptr->key, key) == 0) {
+			if (ptr->next != NULL) {
+				if (prev == NULL) {
+					*dict = ptr->next;
+				} else {
+					prev->next = ptr->next;
+    				}
+			} else if (prev != NULL) {
+				prev->next = NULL;
+			} else {
+				*dict = NULL;
+			}
+
+			free(ptr->key);
+			free(ptr);
+
+			return;
+		}
+	}
+}
+
+void addItem(dict_t **dict, char *key, void *value) {
+	delItem(dict, key); /* If we already have a item with this key, delete it. */
+	dict_t *d = malloc(sizeof(struct dict_t_struct));
+	d->key = malloc(strlen(key)+1);
+	strcpy(d->key, key);
+	d->value = value;
+	d->next = *dict;
+	*dict = d;
+}
+
+// ====================================== End of copied code ========================================
+
+// ==================================================================================================
+// Binary Tree Implementation copied from:
+// https://www.programiz.com/dsa/binary-tree
+
+struct node {
+	int item;
+	struct node* left;
+	struct node* right;
+};
+
+// Inorder traversal
+void inorderTraversal(struct node* root) {
+	if (root == NULL) return;
+	inorderTraversal(root->left);
+	printf("%d ->", root->item);
+	inorderTraversal(root->right);
+}
+
+// Preorder traversal
+void preorderTraversal(struct node* root) {
+	if (root == NULL) return;
+	printf("%d ->", root->item);
+	preorderTraversal(root->left);
+	preorderTraversal(root->right);
+}
+
+// Postorder traversal
+void postorderTraversal(struct node* root) {
+	if (root == NULL) return;
+	postorderTraversal(root->left);
+	postorderTraversal(root->right);
+	printf("%d ->", root->item);
+}
+
+// Create a new Node
+struct node* createNode(value) {
+	struct node* newNode = malloc(sizeof(struct node));
+	newNode->item = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
+
+	return newNode;
+}
+
+// Insert on the left of the node
+struct node* insertLeft(struct node* root, int value) {
+	root->left = createNode(value);
+	return root->left;
+}
+
+// Insert on the right of the node
+struct node* insertRight(struct node* root, int value) {
+	root->right = createNode(value);
+	return root->right;
+}
+
+// ====================================== End of copied code ========================================
+
 
 /* Main Function */
 int main(int argc, char *argv[])
