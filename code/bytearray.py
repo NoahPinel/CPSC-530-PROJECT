b1d89b6f66660b7d055234e7ccccc716a5c572c0
Ty-Irving <ty.irving1@ucalgary.ca>
2023-03-27 15:31:04 -0600

    changes to binaryfile python script just scrictly using bitstring module

--- code/bytearray.py
+++ code/bytearray.py
@@ -3,13 +3,11 @@ f = open("bitcodes", "r")
 
 b = f.read()
 f.close()
-print(b+"\n")
-if len(b) % 8 != 0 :
-    for i in range (len(b) % 8):
-        b = b + "0"
-print(b)
-c = bytearray(int(b[x:x+8], 2) for x in range(0, len(b), 8))
-print(c)
+
+#Dont think this is even needed using bitstring module
+#if len(b) % 8 != 0 :
+#    for i in range (len(b) % 8):
+#        b = b + "0"
 
 with open('binaryfile', 'wb') as f:
     BitArray(bin=b).tofile(f)
